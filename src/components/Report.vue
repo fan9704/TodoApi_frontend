51f8278f8eb59180131b7d05c0e7e6e0440145e2
fan9704 <cxz123499@gmail.com>
2021-12-02 23:36:55 +0800

    Complete ReportChart

--- src/components/Report.vue
+++ src/components/Report.vue
@@ -51,32 +51,35 @@
   <div class="container-fluid ReportBody">
     <h2 class="title">Net profit for the period</h2>
     <div class="row">
+      <div class="col-6">
+        <canvas id="singleprofit" width="400" height="200"></canvas>
+      </div>
+      <div class="col-6">
+        <canvas id="example" width="400" height="200"></canvas>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-2">ID</div>
       <div class="col-2">Product</div>
-      <div class="col-2">Cost</div>
-      <div class="col-2">Price</div>
       <div class="col-2">Quantity</div>
+      <div class="col-2">Sell Date</div>
       <div class="col-2">Profit</div>
     </div>
     <div
       class="container-fluid"
       :key="index"
-      v-for="(product, index) in products"
+      v-for="(product, index) in Records"
     >
       <div class="row">
+        <div class="col-2">{{ product.id }}</div>
         <div class="col-2">{{ product.name }}</div>
-        <div class="col-2">{{ product.cost }}</div>
-        <div class="col-2">{{ product.price }}</div>
-        <div class="col-2">{{ product.quantity }}</div>
-        <input
-          class="col-2"
-          v-bind:value="(product.price - product.cost) * product.quantity"
-          readonly
-          disabled
-        />
+        <div class="col-2">{{ product.sell_quantity }}</div>
+        <div class="col-2">{{ product.sellDate }}</div>
+        <input class="col-2" v-bind:value="product.profit" readonly disabled />
         <div class="col-2" v-show="false">
-          {{
-            (total[index] = (product.price - product.cost) * product.quantity)
-          }}
+          {{ (totalrecord[index] = product.profit) }}
+          {{ (totalrecordname[index] = product.name) }}
+          {{ (singlerecord[index] = product.profit / product.sell_quantity) }}
         </div>
       </div>
     </div>
@@ -90,7 +93,7 @@
           class="col-2 totalexpectedprofit"
           readonly
           disabled
-          v-bind:value="calculate"
+          v-bind:value="calculateprofit"
         />
         <div class="col-2"></div>
       </div>
@@ -101,13 +104,17 @@
 
 <script>
 import axios from "axios";
-
+import Chart from "chart.js/dist/chart.js";
 export default {
-  
   data() {
     return {
       products: [],
       total: [],
+      totalname: [],
+      Records: [],
+      totalrecord: [],
+      totalrecordname: [],
+      singlerecord:[],
     };
   },
   computed: {
@@ -118,14 +125,25 @@ export default {
       }
       return sum;
     },
+    calculateprofit() {
+      let sum = 0;
+      for (let i = 0; i < this.totalrecord.length; i++) {
+        sum += parseInt(this.totalrecord[i]);
+      }
+      return sum;
+    },
   },
-  watch: { /* 只要 chartData 改變，就要重新渲染圖表 */
-  chartData() {
-    this.$data._chart.destroy();  /* 官方文件 api 提供的 destroy() 方法 */
-    this.renderChart(this.chartData, this.options); /* 重新渲染圖表的 function */
+  watch: {
+    /* 只要 chartData 改變，就要重新渲染圖表 */
+    chartData() {
+      this.$data._chart.destroy(); /* 官方文件 api 提供的 destroy() 方法 */
+      this.renderChart(
+        this.chartData,
+        this.options
+      ); /* 重新渲染圖表的 function */
+    },
+    deep: true,
   },
-  deep: true,
-},
   created() {
     axios
       .get("/api/Products")
@@ -133,8 +151,68 @@ export default {
         this.products = response.data;
       })
       .catch((error) => console.log(error));
+
+    axios
+      .get("/api/SellRecords")
+      .then((response) => {
+        this.Records = response.data;
+      })
+      .catch((error) => console.log(error));
   },
-  mounted() {},
+  updated() {
+    let ctx = document.getElementById("example");
+    new Chart(ctx, {
+      type: "bar", // 圖表類型
+      data: {
+        labels: this.totalrecordname, // 標題
+        datasets: [
+          {
+            label: "# of Revenue", // 標籤
+            data: this.totalrecord, // 資料
+            backgroundColor: [
+              // 背景色
+              "rgb(0, 255, 170)",
+            ],
+            borderWidth: 5, // 外框寬度
+          },
+        ],
+      },
+       options: {
+          plugins: {
+                title: {
+                  display: true,
+                  text: 'Per type Product profit',
+                },
+        }
+       }
+    });
+    let ctx2 = document.getElementById("singleprofit");
+    new Chart(ctx2, {
+      type: "bar", // 圖表類型
+      data: {
+        labels: this.totalrecordname, // 標題
+        datasets: [
+          {
+            label: "# of Revenue", // 標籤
+            data: this.singlerecord, // 資料
+            backgroundColor: [
+              // 背景色
+              "rgb(190, 233, 72)",
+            ],
+            borderWidth: 5, // 外框寬度
+          },
+        ],
+      },
+          options: {
+          plugins: {
+                title: {
+                  display: true,
+                  text: 'Per Product profit',
+                },
+        }
+       }
+    });
+  },
   name: "ReportView",
 };
 </script>
@@ -142,6 +220,7 @@ export default {
 h2.title {
   text-align: center;
   margin: 20px;
+  /* color: rgb(190, 233, 72); */
 }
 input.totalexpectedprofit {
   background: linear-gradient(rgb(30, 255, 30), rgb(0, 255, 13));
