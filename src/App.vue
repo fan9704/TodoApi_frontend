0135b9129ff64be636db868dfcbc0d25d860c8d3
fan9704 <cxz123499@gmail.com>
2021-12-16 16:07:22 +0800

    nav-bar Create logout feature

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,126 @@
 <template>
-<nav class="navbar navbar-expand-lg navbar-light bg-light" id="nav-bar">
-  <div class="container-fluid">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" id="nav-bar">
+    <div class="container-fluid">
       <a class="navbar-brand" href="#">
-      <img src="./assets/logo.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
-      Shopping Mall!
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-       <div class="navbar-nav">
-         <router-link class="nav-link" to="/">Home</router-link> 
-         <!-- <router-link class="nav-link" to="/todoitems">TodoItems</router-link>  -->
-         <router-link class="nav-link" to="/products">Products</router-link> 
-         <router-link class="nav-link" to="/Sell">Sell</router-link> 
-         <router-link class="nav-link" to="/Report">Report</router-link> 
-      </div>
-      <div class="navbar-nav nav-right">
-        <router-link class="nav-link" to="/Login">Login</router-link> 
+        <img
+          src="./assets/logo.png"
+          alt=""
+          width="30"
+          height="24"
+          class="d-inline-block align-text-top"
+        />
+        Shopping Mall!
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="navbar-nav">
+          <router-link class="nav-link" to="/">Home</router-link>
+          <!-- <router-link class="nav-link" to="/todoitems">TodoItems</router-link>  -->
+          <router-link class="nav-link" to="/products">Products</router-link>
+          <router-link class="nav-link" to="/Sell">Sell</router-link>
+          <router-link class="nav-link" to="/Report">Report</router-link>
+        </div>
+        <div class="navbar-nav nav-right">
+          <router-link class="nav-link" to="/Login" v-if="!login"
+            >Login!</router-link
+          >
+          <div class="dropdown" v-else>
+            <button
+              class="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Hello {{ username }}!
+            </button>
+            <ul
+              class="dropdown-menu dropdown-menu-end"
+              width="auto"
+              aria-labelledby="dropdownMenuButton1"
+            >
+              <li>
+                <router-link to="/Login" class="dropdown-item" href="#"
+                  >Setting</router-link
+                >
+              </li>
+              <li>
+                <a class="dropdown-item" href="#" v-on:click="logout">Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</nav>
+  </nav>
 
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
-@media (min-width:1000px){
-.nav-right{
-  position: absolute;
-  right: 0px;
- }
+@media (min-width: 1000px) {
+  .nav-right {
+    position: absolute;
+    right: 0px;
+  }
 }
 </style>
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      login: false,
+    };
+  },
+  methods: {
+    getCookie(cname) {
+      var name = cname + "=";
+      var decodedCookie = decodeURIComponent(document.cookie);
+      var ca = decodedCookie.split(";");
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == " ") {
+          c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+      return "";
+    },
+    logout() {
+      this.axios.get("/api/Account/Logout")
+      .then((response)=>{
+        console.log(response)
+        this.login = false;
+        alert("Logout Success");
+      })
+      .catch((error)=>{
+        console.log(error)
+      })
+        
+      
+    },
+  },
+
+  beforeMount() {
+    this.username = this.getCookie("User");
+    if (this.username !== undefined) {
+      this.login = true;
+    } else {
+      this.login = false;
+    }
+  },
+};
+</script>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,78 +1,84 @@
 <template>
   <div class="container-fluid form-body">
     <transition name="slide">
-    <div class="container-fluid login-form" v-show="isShow">
-      <h2 class="title">Login Now!</h2>
-      <div class="form-floating row form-border">
-        <input
-          type="text"
-          class="form-control"
-          id="floatingInput2"
-          placeholder="Username"
-          v-model="username"
-        />
-        <label for="floatingInput2">Username</label>
+      <div class="container-fluid login-form" v-show="isShow">
+        <h2 class="title">Login Now!</h2>
+        <p class="error" v-if="errorMsg1">Username Or Password Not Correct</p>
+        <div class="form-floating row form-border">
+          <input
+            type="text"
+            class="form-control"
+            id="floatingInput2"
+            placeholder="Username"
+            v-model="username"
+          />
+          <label for="floatingInput2">Username</label>
+        </div>
+        <div class="form-floating row form-border">
+          <input
+            type="password"
+            class="form-control"
+            id="floatingInput"
+            placeholder="password"
+            v-model="password"
+          />
+          <label for="floatingInput">Password</label>
+        </div>
+        <div class="row form-border">
+          <button class="col-6 btn btn-info" v-on:click="reset">Reset</button>
+          <button class="col-6 btn btn-info" v-on:click="login">Login</button>
+        </div>
+        <div class="row" style="text-align: center">
+          <div class="col-12" v-on:click="slidetoggle">
+            Hasn,t Account <a href="#"> Register Now!!</a>
+          </div>
+        </div>
       </div>
-      <div class="form-floating row form-border">
-        <input
-          type="password"
-          class="form-control"
-          id="floatingInput"
-          placeholder="password"
-          v-model="password"
-        />
-        <label for="floatingInput">Password</label>
-      </div>
-      <div class="row form-border">
-        <button class="col-6 btn btn-info" v-on:click="reset">Reset</button>
-        <button class="col-6 btn btn-info" v-on:click="login">Login</button>
-      </div>
-      <div class="row" style="text-align: center">
-        <div class="col-12" v-on:click="slidetoggle">Hasn,t Account <a href="#"> Register Now!!</a></div>
-      </div>
-    </div>
     </transition>
     <!-- register form -->
     <transition name="slide">
-    <div class="container-fluid register-form" v-show="!isShow">
-      <h2 class="title">Register Now!</h2>
-      <div class="form-floating row form-border">
-        <input
-          type="text"
-          class="form-control"
-          id="floatingInput2"
-          placeholder="Username"
-          v-model="username"
-        />
-        <label for="floatingInput2">Username</label>
+      <div class="container-fluid register-form" v-show="!isShow">
+        <h2 class="title">Register Now!</h2>
+        <p class="error" v-if="errorMsg2"></p>
+        <div class="form-floating row form-border">
+          <input
+            type="text"
+            class="form-control"
+            id="floatingInput2"
+            placeholder="Username"
+            v-model="username"
+          />
+          <label for="floatingInput2">Username</label>
+        </div>
+        <div class="form-floating row form-border">
+          <input
+            type="password"
+            class="form-control"
+            id="floatingInput"
+            placeholder="Psassword"
+            v-model="password"
+          />
+          <label for="floatingInput">Password</label>
+        </div>
+        <div class="row form-border">
+          <button class="col-6 btn btn-info" v-on:click="reset">Reset</button>
+          <button class="col-6 btn btn-info" v-on:click="register">
+            Register
+          </button>
+        </div>
+        <div class="row" style="text-align: center">
+          <div class="col-12" v-on:click="slidetoggle">
+            Has Account<a href="#"> Login Now!!</a>
+          </div>
+        </div>
       </div>
-      <div class="form-floating row form-border">
-        <input
-          type="password"
-          class="form-control"
-          id="floatingInput"
-          placeholder="Psassword"
-          v-model="password"
-        />
-        <label for="floatingInput">Password</label>
-      </div>
-      <div class="row form-border">
-        <button class="col-6 btn btn-info" v-on:click="reset">Reset</button>
-        <button class="col-6 btn btn-info" v-on:click="register">
-          Register
-        </button>
-      </div>
-      <div class="row" style="text-align: center">
-        <div class="col-12" v-on:click="slidetoggle">Has Account<a href="#"> Login Now!!</a></div>
-      </div>
-    </div>
     </transition>
   </div>
 </template>
 
 <style scoped>
 .form-body {
-  background: linear-gradient(rgb(127, 255, 255), white,rgb(127, 255, 255));
+  background: linear-gradient(rgb(127, 255, 255), white, rgb(127, 255, 255));
   padding: 20px;
   height: 90vh;
 }
@@ -80,31 +86,35 @@ h2.title {
   text-align: center;
   color: rgb(83, 83, 83);
 }
+p.error{
+  text-align: center;
+  color:rgb(248, 86, 86);
+}
 .login-form {
-  width: 70%; 
+  width: 70%;
   margin: auto;
   border-radius: 5px;
   border: 3px solid aqua;
   padding: 10px;
   background: linear-gradient(rgb(129, 236, 236), rgb(42, 191, 236));
-  position: absolute; 
-   left: 10%;
-  right: 10%; 
+  position: absolute;
+  left: 10%;
+  right: 10%;
 }
 .register-form {
-  width: 70%; 
+  width: 70%;
   margin: auto;
   border-radius: 5px;
   border: 3px solid aqua;
   padding: 10px;
   background: linear-gradient(rgb(129, 236, 236), rgb(42, 191, 236));
-  position: absolute; 
-   left: 10%;
-  right: 10%; 
+  position: absolute;
+  left: 10%;
+  right: 10%;
 }
-.col-12 a{
+.col-12 a {
   text-decoration: none;
-  color:rgb(68, 104, 221);
+  color: rgb(68, 104, 221);
 }
 .slide-leave-active,
 .slide-enter-active {
@@ -113,30 +123,30 @@ h2.title {
 }
 .slide-enter-from {
   transform: translateY(100%);
-  opacity:0;
+  opacity: 0;
 }
 .slide-enter-to {
   /* transform: translateY(-50%); */
-  opacity:1;
+  opacity: 1;
 }
 .slide-leave-from {
   /* transform: translateY(-50%); */
-   opacity:1;
+  opacity: 1;
 }
 .slide-leave-to {
   transform: translateY(100%);
-   opacity:0;
+  opacity: 0;
 }
-
 </style>
 <script>
-// import axios from "axios";
 export default {
   data() {
     return {
       username: "",
       password: "",
-      isShow:true,
+      isShow: true,
+      errorMsg1:false,
+      errorMsg2:false,
     };
   },
   methods: {
@@ -154,35 +164,40 @@ export default {
         .then((response) => {
           if (response.data == true) {
             alert("Login Success");
-          }else{
-             alert("Login Fail");
+            window.location.href("/");
+          } else {
+            this.errorMsg1=true;
           }
         })
 
         .catch((error) => console.log(error));
     },
-    register(){
+    register() {
       let config = {
         username: this.username,
         password: this.password,
       };
-      this.axios
-        .post("/api/Users", config)
-        .then((response) => {
-          console.log(response.status);
-          if (response.status == 201) {
-            this.isShow=!this.isShow;
-            alert("Register Success");
-          }else{
-             alert("Register Fail");
-          }
-        })
+      if (this.username == "" || this.password == "") {
+        alert("Username Or Password cannot be empty");
+      } else {
+        this.axios
+          .post("/api/Users", config)
+          .then((response) => {
+            console.log(response.status);
+            if (response.status == 201) {
+              this.isShow = !this.isShow;
+              alert("Register Success");
+            } else {
+              alert("Register Fail");
+            }
+          })
 
-        .catch((error) => console.log(error));
+          .catch((error) => console.log(error));
+      }
     },
-    slidetoggle(){
-      this.isShow=!this.isShow;
-    }
+    slidetoggle() {
+      this.isShow = !this.isShow;
+    },
   },
   name: "LoginView",
 };
